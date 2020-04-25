536e344227cf0dfd582e4baf7f4107448a8ed6bc
loresa99 <63755609+loresa99@users.noreply.github.com>
2020-04-25 12:43:43 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+	<title><B> <i> <u> Paris-Capitala muzeelor unice </u> </i> </B></title>
+	<?php
+		include 'db_connection.php';
+		initializeDatabase();
+	?>
+</head>
+<frameset rows="15%,*">
+	<frame name="header" src="header/header.php"> </frame>
+	<frame name="content" src="content.html"> </frame>
+</frameset>
