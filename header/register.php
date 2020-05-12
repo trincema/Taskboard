ed759f5ddfb6f614086f90c45a85a857df07e83d
manuel <emanuel.trinc@yahoo.com>
2020-05-12 12:23:03 +0300

    Implement register validators

--- header/register.php
+++ header/register.php
@@ -1,5 +1,6 @@
 <?php
 	include "../db_connection.php";
+	$register_err = "";
 	if($_SERVER["REQUEST_METHOD"] == "POST") {
 		session_start();
 		$first_name = $_POST['First_Name'];
@@ -65,7 +66,7 @@
 					header("location: http://localhost/taskboard/header/login.php");
 				}
 			} else {
-				echo"User already exists";
+				$register_err = "User already exists";
 			}
 		}
 		mysqli_close($connection);
@@ -81,66 +82,99 @@
 	<link rel="stylesheet" type="text/css" href="register.css">
 	<script type="text/javascript" src="register.js"></script>
 	<!-- Bootstrap CSS -->
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script> 
 </head>
 <body>
 	<div class="signup-form">
-		<form method="post" action="">
+		<form method="post" class="needs-validation" action="" novalidate>
 		<h2 class="text-center">Sign Up</h2>
 			<!-- First name -->
 			<div class="form-group">
 				<div class="input-group">
-					<span class="input-group-addon">
-						<span style="display: inline-block; width:2em;"><i class="fa fa-user"></i></span>
-					</span>
-					<input type="text" class="form-control" name="First_Name" placeholder="First Name" required="required">
+					<div class="input-group-prepend">
+          				<span class="input-group-text" id="firstnamePrepend" style="display: inline-block; width: 3em;"><i class="fa fa-user"></i></span>
+        			</div>
+					<input type="text" class="form-control" name="First_Name" placeholder="First name..."
+						id="username" aria-describedby="firstnamePrepend" minlength="3" required>
+					<div class="valid-feedback">
+        				Looks good!
+      				</div>
+					<div class="invalid-feedback">
+        				The minimum length of First name must be 3!
+      				</div>
 				</div>
 			</div>
 			<!-- Last name -->
 			<div class="form-group">
 				<div class="input-group">
-					<span class="input-group-addon">
-						<span style="display: inline-block; width:2em;"><i class="fa fa-user"></i></span>
-					</span>
-					<input type="text" class="form-control" name="Last_Name" placeholder="Last Name" required="required">
+					<div class="input-group-prepend">
+          				<span class="input-group-text" id="lastnamePrepend" style="display: inline-block; width: 3em;"><i class="fa fa-user"></i></span>
+        			</div>
+					<input type="text" class="form-control" name="Last_Name" placeholder="Last name..."
+						id="username" aria-describedby="lastnamePrepend" minlength="3" required>
+					<div class="valid-feedback">
+        				Looks good!
+      				</div>
+					<div class="invalid-feedback">
+        				The minimum length of Last name must be 3!
+      				</div>
 				</div>
 			</div>
 			<!-- Email address -->
 			<div class="form-group">
 				<div class="input-group">
-					<span class="input-group-addon">
-						<span style="display: inline-block; width:2em;"><i class="fa fa-envelope"></i></span>
-					</span>
-					<input type="text" class="form-control" name="Email" placeholder="Email" required="required">
+					<div class="input-group-prepend">
+          				<span class="input-group-text" id="emailPrepend" style="display: inline-block; width: 3em;"><i class="fa fa-user"></i></span>
+        			</div>
+					<input type="text" class="form-control" name="Email" placeholder="Email.."
+						id="username" aria-describedby="emailPrepend" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$" required>
+					<div class="valid-feedback">
+        				Looks good!
+      				</div>
+					<div class="invalid-feedback">
+        				Please enter a proper email address!
+      				</div>
 				</div>
 			</div>
 			<!-- Password -->
 			<div class="form-group">
 				<div class="input-group">
-					<span class="input-group-addon">
-						<span style="display: inline-block; width:2em;"><i class="fa fa-lock"></i></span>
-					</span>
-					<input type="password" class="form-control" name="Password" placeholder="Password" required="required">
+					<div class="input-group-prepend">
+          				<span class="input-group-text" id="passwordPrepend" style="display: inline-block; width: 3em;"><i class="fa fa-lock"></i></span>
+        			</div>
+					<input type="password" class="form-control" name="Password" placeholder="Password.."
+						aria-describedby="passwordPrepend" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" required>
+					<div class="valid-feedback">
+        				Looks good!
+      				</div>
+					<div class="invalid-feedback">
+        				The password must contain at least a lowercase letter, a capital (uppercase) letter, a number, and minimum 8 characters!
+      				</div>
 				</div>
 			</div>
 			<!-- Confirm Password -->
 			<div class="form-group">
 				<div class="input-group">
-					<span class="input-group-addon">
-						<span style="display: inline-block; width:2em;"><i class="fa fa-lock"></i></span>
-					</span>
-					<input type="password" class="form-control" name="Confirm_Password" placeholder="Confirm Password" required="required">
+					<div class="input-group-prepend">
+          				<span class="input-group-text" id="confirmpasswordPrepend" style="display: inline-block; width: 3em;"><i class="fa fa-lock"></i></span>
+        			</div>
+					<input type="password" class="form-control" name="Confirm_Password" placeholder="Confirm password.."
+						aria-describedby="confirmpasswordPrepend" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" required>
+					<div class="valid-feedback">
+        				Looks good!
+      				</div>
+					<div class="invalid-feedback">
+        				The password must contain at least a lowercase letter, a capital (uppercase) letter, a number, and minimum 8 characters!
+      				</div>
 				</div>
 			</div>
 			<!-- Skill -->
 			<div class="form-group">
 				<div class="input-group">
-					<span class="input-group-addon">
-						<span style="display: inline-block; width: 8em; text-align: left;"> <i class="fa fa-cogs"></i> Skill</span>
-					</span>
+					<div class="input-group-prepend">
+          				<span class="input-group-text" id="confirmpasswordPrepend" style="width: 9em;"><i class="fa fa-cogs"> Skill</i></span>
+        			</div>
 					<select class="form-control" name="Skill">
 						<option>C</option>
 						<option>C++</option>
@@ -151,9 +185,9 @@
 			<!-- Nivel Skill -->
 			<div class="form-group">
 				<div class="input-group">
-					<span class="input-group-addon">
-						<span style="display: inline-block; width: 8em; text-align: left;"> <i class="fa fa-arrow-up"></i> Skill Level</span>
-					</span>
+					<div class="input-group-prepend">
+          				<span class="input-group-text" id="confirmpasswordPrepend" style="width: 9em;"><i class="fa fa-arrow-up"> Skill Level</i></span>
+        			</div>
 					<select class="form-control" name="SkillLevel">
 						<option>Level 1</option>
 						<option>Level 2</option>
@@ -171,9 +205,9 @@
 			<!-- Working Hours -->
 			<div class="form-group">
 				<div class="input-group">
-					<span class="input-group-addon">
-						<span style="display: inline-block; width: 8em; text-align: left;"> <i class="fa fa-clock-o"></i> Working Hours</span>
-					</span>
+					<div class="input-group-prepend">
+          				<span class="input-group-text" id="confirmpasswordPrepend" style="width: 9em;"><i class="fa fa-clock-o"> Working Hours</i></span>
+        			</div>
 					<select class="form-control" name="WorkingHours">
 						<option>4h/day</option>
 						<option>6h/day</option>
@@ -187,5 +221,9 @@
 		</form>
 		<p class="text-center text-muted small">Already have an account? <a href="http://localhost/Taskboard/header/login.php">Sign in here!</a></p>
 	</div>
+
+	<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 </body>
 </html>
