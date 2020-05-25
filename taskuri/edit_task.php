b6ef6c9123f8e93a570f58fdf482ba7c92931256
manuel <emanuel.trinc@yahoo.com>
2020-05-25 11:40:53 +0300

    Fix edit task issue

--- taskuri/edit_task.php
+++ taskuri/edit_task.php
@@ -33,7 +33,8 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
 				$assigned_to = $_POST['EditAssignedTo'];
 				$status = $_POST['EditStatus'];
 			}
-			else{
+			else {
+				$id = $_POST['EditTaskId'];
 				$duration = $_POST['EditDuration'];
 				$status = $_POST['EditStatus'];
 			}
@@ -48,26 +49,6 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
 	if(!$connection) {
 		echo "Database Connection Error...".mysqli_connect_error();
 	} else {
-		$sql="SELECT * FROM $database.Skills WHERE skill='$skill'";
-		$retval = mysqli_query( $connection, $sql );
-		if(! $retval ) {
-			echo "Error access in table Skills".mysqli_error($connection);
-		}
-		if (mysqli_num_rows($retval) == 1) {
-			while($row = mysqli_fetch_assoc($retval)) {
-				$skill_id = $row["id"];
-			}
-		}
-		$sql="SELECT * FROM $database.SkillLevel WHERE skill_level='$skill_level'";
-		$retval = mysqli_query( $connection, $sql );
-		if(! $retval ) {
-			echo "Error access in table SkillLevel".mysqli_error($connection);
-		}
-		if (mysqli_num_rows($retval) == 1) {
-			while($row = mysqli_fetch_assoc($retval)) {
-				$level_id = $row["id"];
-			}
-		}
 		$sql="SELECT * FROM $database.TaskStatus WHERE task_status='$status'";
 		$retval = mysqli_query( $connection, $sql );
 		if(! $retval ) {
@@ -79,25 +60,53 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
 			}
 		}
 
-		$pieces = explode(" ", $assigned_to);
-		$first_name = $pieces[0];
-		$last_name = $pieces[1];
-		$sql = "SELECT * FROM $database.TeamMembers WHERE first_name='$first_name' AND last_name='$last_name'";
-		$retval = mysqli_query( $connection, $sql );
-		if(! $retval ) {
-			echo "Error access in table TeamMembers".mysqli_error($connection);
-		}
-		if (mysqli_num_rows($retval) == 1) {
-			while($row = mysqli_fetch_assoc($retval)) {
-				$user_id = $row["id"];
+		if ($role == 'Admin') {
+			$sql="SELECT * FROM $database.Skills WHERE skill='$skill'";
+			$retval = mysqli_query( $connection, $sql );
+			if(! $retval ) {
+				echo "Error access in table Skills".mysqli_error($connection);
+			}
+			if (mysqli_num_rows($retval) == 1) {
+				while($row = mysqli_fetch_assoc($retval)) {
+					$skill_id = $row["id"];
+				}
+			}
+			$sql="SELECT * FROM $database.SkillLevel WHERE skill_level='$skill_level'";
+			$retval = mysqli_query( $connection, $sql );
+			if(! $retval ) {
+				echo "Error access in table SkillLevel".mysqli_error($connection);
+			}
+			if (mysqli_num_rows($retval) == 1) {
+				while($row = mysqli_fetch_assoc($retval)) {
+					$level_id = $row["id"];
+				}
+			}
+			$pieces = explode(" ", $assigned_to);
+			$first_name = $pieces[0];
+			$last_name = $pieces[1];
+			$sql = "SELECT * FROM $database.TeamMembers WHERE first_name='$first_name' AND last_name='$last_name'";
+			$retval = mysqli_query( $connection, $sql );
+			if(! $retval ) {
+				echo "Error access in table TeamMembers".mysqli_error($connection);
+			}
+			if (mysqli_num_rows($retval) == 1) {
+				while($row = mysqli_fetch_assoc($retval)) {
+					$user_id = $row["id"];
+				}
+			}
+			$sql = "UPDATE Taskboard.Tasks SET task_name='$task_name',skill_required=$skill_id,level_required=$level_id,duration=$duration,".
+					"task_status=$status_id,assigned_member=$user_id WHERE id=$id";
+			$retval = mysqli_query( $connection, $sql );
+			if(! $retval ) {
+				echo"Error access in table TeamMembers".mysqli_error($connection);
+			}
+		} else {
+			$sql = "UPDATE Taskboard.Tasks SET duration=$duration,task_status=$status_id WHERE id=$id";
+			$retval = mysqli_query( $connection, $sql );
+			if(! $retval ) {
+				echo"Error access in table TeamMembers".mysqli_error($connection);
 			}
 		}
-		$sql = "UPDATE Taskboard.Tasks SET duration=$duration,".
-				"task_status=$status_id WHERE id=$id";
-		$retval = mysqli_query( $connection, $sql );
-		if(! $retval ) {
-			echo"Error access in table TeamMembers".mysqli_error($connection);
-        }
         mysqli_close($connection);
 	}
 }
